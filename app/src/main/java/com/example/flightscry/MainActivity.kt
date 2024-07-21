3d1dbe237b133475b02ccc7e78c800e94d911bbf
DracoRX16 <aayushjalgaonkar14@gmail.com>
2024-07-22 00:16:22 +0530

    Created mobile flight itinerary

---
+++ app/src/main/java/com/example/flightscry/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.flightscry
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.flightskry.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
